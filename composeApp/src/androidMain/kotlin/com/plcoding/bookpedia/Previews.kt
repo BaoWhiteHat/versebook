19cbdf7cd9974cee8aa9fbf241734366acb779ea
lequocbao2222005@gmail.com <145678921+BaoWhiteHat@users.noreply.github.com>
2026-03-18 00:16:27 +0700

    (feat): Update Material and BookListScreen 1
    - Update the material into material 3
    - Update the UI (add color, animation) for the screen
    - Fix bugs
    - Create String xml file to easy manage and maintain files

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -1,25 +1,35 @@
 package com.plcoding.bookpedia
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
-
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxWidth
-
+import androidx.compose.ui.graphics.Color
 
 
 @Preview
 @Composable
 private fun BookSearchBarPreview() {
-    MaterialTheme {
-        BookSearchBar(
-            searchQuery = "Kotlin",
-            onSearchQueryChange = {},
-            onImeSearch = {},
+
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
+                .background(Color.White)
         )
-    }
+
+        {
+            BookSearchBar(
+                searchQuery = "",
+                onSearchQueryChange = {},
+                onImeSearch = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+        }
+
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/BookListAction.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/BookListAction.kt
@@ -1,9 +1,39 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
 
 sealed interface BookListAction {
     data class OnSearchQueryChange(val query: String) : BookListAction
     data class OnBookClick(val book: Book) : BookListAction
     data class OnTabSelected(val index: Int) : BookListAction
+}
+
+@Composable
+private fun BookListScreen(
+    state: BookListState,
+    onAction: (BookListAction) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            }
+        )
+    }
 }
