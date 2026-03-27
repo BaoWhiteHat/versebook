d864ad3355ca28126235a798999ff0be98ead343
lequocbao2222005@gmail.com <145678921+BaoWhiteHat@users.noreply.github.com>
2026-03-27 18:08:25 +0700

    (feat): Create BookListItem
    Create function to load book categories

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,11 +1,18 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.runtime.*
+import com.plcoding.bookpedia.book.presentation.BookListScreenRoot
+import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 @Composable
 @Preview
 fun App() {
+    BookListScreenRoot(
+        viewModel = remember{ BookListViewModel() },
+        onBookClick = {
 
+        }
+    )
 }
